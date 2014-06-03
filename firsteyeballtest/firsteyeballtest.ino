b8514bb76215c38d3d22e183cc1283806cba1873
Georgia Reh <georgia.reh@gmail.com>
2014-06-03 15:11:58 -0700

    the eyes can see ambient light

--- firsteyeballtest/firsteyeballtest.ino
+++ firsteyeballtest/firsteyeballtest.ino
@@ -4,7 +4,7 @@ int val = 0;
 int valnew= 0;
 
 void setup(){
-  pinMode(eyeball, OUTPUT);
+  pinMode(eyeball, INPUT);
   pinMode(light, OUTPUT);
   Serial.begin(9600);
 }
@@ -12,6 +12,6 @@ void setup(){
 void loop(){
   val = analogRead(eyeball);
   Serial.println(val);
-  valnew = map(val, 98, 102, 0, 255);
+  valnew = map(val, 0, 175, 0, 255);
   analogWrite(light, valnew);
 }
