f5015ec43f9cc8d75e5c964d8d88633e244e8a6b
Georgia Reh <georgia.reh@gmail.com>
2014-07-26 11:48:52 -0700

    it compiles! needs to be tested on hardware

--- twohbridge/twohbridge.ino
+++ twohbridge/twohbridge.ino
@@ -3,23 +3,26 @@ int LTR = 3; //PWM
 int LBL = 11; //digital pin
 int LBR = 12; //digital pin
 
-int RTL = ; //PWM
-int RTR = ; //PWM
-int RBL = ; //digital pin
-int RBR = ; //digital pin
+int RTL = 6; //PWM
+int RTR = 9; //PWM
+int RBL = 8; //digital pin
+int RBR = 13; //digital pin
 
 int eyeballLeft = A1; //analog read
-int eyeballRight = ; //analog read
+int eyeballRight = A2; //analog read
+int Malp = 0;
 
 int brainLeft = 0;
 int brainRight = 0;
 
 void setup(){
+  //This is all of the pins controlling the left motor 
   pinMode(LTL, OUTPUT);
   pinMode(LTR, OUTPUT);
   pinMode(LBL, OUTPUT);
   pinMode(LBR, OUTPUT);
   
+  //This is all of the pins controlling the right motor
   pinMode(RTL, OUTPUT);
   pinMode(RTR, OUTPUT);
   pinMode(RBL, OUTPUT);
@@ -40,12 +43,17 @@ void loop(){
   analogWrite(LTR, 0);
   analogWrite(RTL, 0);
   analogWrite(RTR, 0);
+  
   //look around
   brainLeft = analogRead(eyeballLeft);
   brainRight = analogRead(eyeballRight);
+  
   //map brain inputs to motor numbers 
+  Malp = brainRight - brainLeft;
+  
   //react
   if (brainLeft > 2 * brainRight){
+    //case 1: sharp left turn
     //safety
     digitalWrite(LBL, HIGH); //NPN
     digitalWrite(RBL, HIGH); //NPN
@@ -55,10 +63,11 @@ void loop(){
     digitalWrite(LBR, LOW); //NPN
     digitalWrite(RBR, LOW); //NPN
     analogWrite(LTL, brainRight); //PNP
-    analogWrite(RTL, Malp); //PNP how calculate Malp??? need testing
+    analogWrite(RTL, Malp); //how fast should this motor spin backwards
     delay(1000);
   }
-  elif (brainLeft > brainRight){
+  else if (brainLeft > brainRight){
+    //case 2: soft left turn 
     //safety
     digitalWrite(LBL, HIGH); //NPN
     digitalWrite(RBR, HIGH); //NPN
@@ -71,7 +80,8 @@ void loop(){
     analogWrite(RTR, brainLeft); //PNP
     //delay(1000);
   }
-  elif (brainRight > 2 * brainLeft){
+  else if (brainRight > 2 * brainLeft){
+    //case 3: sharp turn to the right
     //safety
     digitalWrite(RBR, HIGH); //NPN
     digitalWrite(LBR, HIGH); //NPN
@@ -81,10 +91,11 @@ void loop(){
     digitalWrite(RBL, LOW); //NPN
     digitalWrite(LBL, LOW); //NPN
     analogWrite(RTR, brainRight); //PNP
-    analogWrite(LTR, Malp); //PNP how calculate Malp??? need testing
+    analogWrite(LTR, Malp); //how fast should this motor spin backwards? 
     //delay(1000);
   }
-  elif (brainRight > brainLeft){
+  else if (brainRight > brainLeft){
+    //case 4: soft turn to the left 
     //safety
     digitalWrite(RBL, HIGH); //NPN
     digitalWrite(LBR, HIGH); //NPN
@@ -97,6 +108,10 @@ void loop(){
     analogWrite(LTR, brainLeft); //PNP
     //delay(1000);
   }
+  else {
+  //delay(1);
+  }
+}
   
     
     
